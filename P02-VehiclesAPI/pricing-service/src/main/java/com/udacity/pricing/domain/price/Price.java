1e173b0d4c9f18ec0d6f1ed64902ac8969f66db3
borgir <ricardofmiguel@gmail.com>
2021-05-03 02:23:44 +0100

    Convert the pricing service to a microservice

--- P02-VehiclesAPI/pricing-service/src/main/java/com/udacity/pricing/domain/price/Price.java
+++ P02-VehiclesAPI/pricing-service/src/main/java/com/udacity/pricing/domain/price/Price.java
@@ -1,12 +1,19 @@
 package com.udacity.pricing.domain.price;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import java.math.BigDecimal;
+import javax.persistence.Id;
 
 /**
  * Represents the price of a given vehicle, including currency.
  */
+@Entity
 public class Price {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private String currency;
     private BigDecimal price;
     private Long vehicleId;
