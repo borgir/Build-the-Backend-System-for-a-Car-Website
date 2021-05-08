9745d34d027690cb1da2bcb6e828c6ebd812d07d
borgir <ricardofmiguel@gmail.com>
2021-05-09 00:51:44 +0100

    Test a particular vehicle ID get

--- P02-VehiclesAPI/pricing-service/src/test/java/com/udacity/pricing/PricingServiceApplicationTests.java
+++ P02-VehiclesAPI/pricing-service/src/test/java/com/udacity/pricing/PricingServiceApplicationTests.java
@@ -1,16 +1,38 @@
 package com.udacity.pricing;
 
+import com.udacity.pricing.api.PricingController;
+import com.udacity.pricing.service.PricingService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.mockito.Mockito.verify;
+import static org.mockito.internal.verification.VerificationModeFactory.times;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@WebMvcTest(PricingController.class)
 public class PricingServiceApplicationTests {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@MockBean
+	PricingService pricingService;
 
 	@Test
 	public void contextLoads() {
+	}
+
+	@Test
+	public void testGetPrice() throws Exception {
+		mockMvc.perform(get("/services/price").param("vehicleId", "9")).andExpect(status().isOk());
 	}
 
 }
